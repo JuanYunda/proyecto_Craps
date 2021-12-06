78d05a21cbf92a2803111360b0a2807ea508383d
202043008 Juan Mazuera <juan.yunda@correounivalle.edu.co>
2021-12-06 14:29:49 -0500

    create Game's Table v1.basic

--- src/juegoCraps/GUI.java
+++ src/juegoCraps/GUI.java
@@ -4,24 +4,32 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
- * This class is used for ...
+ * This class is used as view craps class.
  * @autor Paola-J Rodriguez-C paola.rodriguez@correounivalle.edu.co
- * @version v.1.0.0 date:21/11/2021
+ * @version v.1.0.0 date:06/12/2021
  */
-public class GUI extends JFrame {
+public class GUI extends JFrame
+{
+    private static final String MESAJE_INICIO="";
 
     private Header headerProject;
+    private JLabel dado1, dado2;
+    private JButton lanzar;
+    private JPanel panelDados, panelResultados;
+    private ImageIcon imagenDado;
+    private JTextArea resultados;
 
     /**
      * Constructor of GUI class
      */
-    public GUI(){
+    public GUI()
+    {
         initGUI();
 
         //Default JFrame configuration
-        this.setTitle("The Title app");
-        this.setSize(200,100); //especifica el area de la pestaña
-        //this.pack(); //el area de la pestaña se adapta
+        this.setTitle("Craps Game");
+        //this.setSize(200,100); //especifica el area de la pestaña
+        this.pack(); //el area de la pestaña se adapta
         this.setResizable(true);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
@@ -32,13 +40,36 @@ public class GUI extends JFrame {
      * This method is used to set up the default JComponent Configuration,
      * create Listener and control Objects used for the GUI class
      */
-    private void initGUI() {
+    private void initGUI()
+    {
         //Set up JFrame Container's Layout
         //Create Listener Object and Control Object
-        //Set up JComponents
-        headerProject = new Header("Header ...", Color.BLACK);
 
+        //Set up JComponents
+        headerProject = new Header("Tablero de Juego", Color.BLACK);
         this.add(headerProject,BorderLayout.NORTH); //Change this line if you change JFrame Container's Layout
+
+        imagenDado = new ImageIcon(getClass().getResource("/resources/dado.png"));
+        dado1 = new JLabel(imagenDado);
+        dado2 = new JLabel(imagenDado);
+
+        lanzar = new JButton("Lanzar");
+
+        panelDados = new JPanel();
+        panelDados.setPreferredSize(new Dimension(300,180));
+        panelDados.setBorder(BorderFactory.createTitledBorder("Tus Dados"));
+        panelDados.add(dado1);
+        panelDados.add(dado2);
+        panelDados.add(lanzar);
+
+        this.add(panelDados,BorderLayout.CENTER);
+
+        resultados = new JTextArea(7,31);
+        resultados.setText(MESAJE_INICIO);
+        resultados.setBorder(BorderFactory.createTitledBorder("¿Que debes hacer?"));
+        JScrollPane scroll = new JScrollPane(resultados);
+        this.add(scroll,BorderLayout.EAST);
+
     }
 
     /**
